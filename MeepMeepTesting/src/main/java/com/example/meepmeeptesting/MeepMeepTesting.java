b11a53dd62e75763ce7a43a42862c43f2eafbceb
rawbill <siddharth.goli@gmail.com>
2025-03-06 17:44:29 -0500

    comment out 5+0 meepmeep bot

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -31,57 +31,57 @@ public class MeepMeepTesting {
                 .strafeToLinearHeading(new Vector2d(-17, -11.5), Math.toRadians(180)) // park
                 .build());
 
-        RoadRunnerBotEntity specimen5Bot = new DefaultBotBuilder(meepMeep)
-                .setDimensions(16, 16)
-                .setColorScheme(new ColorSchemeRedDark())
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(70, 70, Math.toRadians(180), Math.toRadians(180), 10)
-                .build();
-
-        specimen5Bot.runAction(specimen5Bot.getDrive().actionBuilder(new Pose2d(15, -62.5, Math.toRadians(90)))
-
-                .strafeToLinearHeading(new Vector2d(3, -28.75), Math.toRadians(90)) // place 1
-
-                .setTangent(Math.toRadians(-90))
-                .splineToConstantHeading(new Vector2d(26, -36), Math.toRadians(0))
-                .splineToSplineHeading(new Pose2d(45, -14, Math.toRadians(180)), Math.toRadians(0))
-                .setTangent(Math.toRadians(-90))
-                .splineToLinearHeading(new Pose2d(45, -54, Math.toRadians(180)), Math.toRadians(-90)) // push 2
-
-//                .setTangent(Math.toRadians(90))
-                .splineToLinearHeading(new Pose2d(43, -30, Math.toRadians(180)), Math.toRadians(90))
-                .splineToLinearHeading(new Pose2d(56, -14, Math.toRadians(180)), Math.toRadians(0))
-                .setTangent(Math.toRadians(-90))
-                .splineToLinearHeading(new Pose2d(56, -54, Math.toRadians(180)), Math.toRadians(-90)) // push 3
-
-//                .setTangent(Math.toRadians(90))
-                .splineToLinearHeading(new Pose2d(54, -30, Math.toRadians(180)), Math.toRadians(90))
-                .splineToLinearHeading(new Pose2d(64.5, -14, Math.toRadians(180)), Math.toRadians(0))
-                .setTangent(Math.toRadians(-90))
-                .splineToLinearHeading(new Pose2d(64.5, -54, Math.toRadians(180)), Math.toRadians(-90)) // push 4
-
-//                .setTangent(Math.toRadians(90))
-                .splineToSplineHeading(new Pose2d(52, -47, Math.toRadians(90)), Math.toRadians(180))
-                .splineToSplineHeading(new Pose2d(46.5, -62, Math.toRadians(90)), Math.toRadians(-90)) // pick 2
-
-                .strafeToLinearHeading(new Vector2d(1, -28.75), Math.toRadians(90)) // place 2
-
-                .strafeToLinearHeading(new Vector2d(46.5, -62), Math.toRadians(90)) // pick 3
-
-                .strafeToLinearHeading(new Vector2d(0.5, -28.75), Math.toRadians(90)) // place 3
-
-                .strafeToLinearHeading(new Vector2d(46.5, -62), Math.toRadians(90)) // pick 4
-
-                .strafeToLinearHeading(new Vector2d(0, -28.75), Math.toRadians(90)) // place 4
-
-                .strafeToLinearHeading(new Vector2d(46.5, -62), Math.toRadians(90)) // pick 5
-
-                .strafeToLinearHeading(new Vector2d(-0.5, -28.75), Math.toRadians(90)) // place 5
-
-                .strafeToLinearHeading(new Vector2d(32, -60), Math.toRadians(90)) // park
-
-
-                .build());
+//        RoadRunnerBotEntity specimen5Bot = new DefaultBotBuilder(meepMeep)
+//                .setDimensions(16, 16)
+//                .setColorScheme(new ColorSchemeRedDark())
+//                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+//                .setConstraints(70, 70, Math.toRadians(180), Math.toRadians(180), 10)
+//                .build();
+//
+//        specimen5Bot.runAction(specimen5Bot.getDrive().actionBuilder(new Pose2d(15, -62.5, Math.toRadians(90)))
+//
+//                .strafeToLinearHeading(new Vector2d(3, -28.75), Math.toRadians(90)) // place 1
+//
+//                .setTangent(Math.toRadians(-90))
+//                .splineToConstantHeading(new Vector2d(26, -36), Math.toRadians(0))
+//                .splineToSplineHeading(new Pose2d(45, -14, Math.toRadians(180)), Math.toRadians(0))
+//                .setTangent(Math.toRadians(-90))
+//                .splineToLinearHeading(new Pose2d(45, -54, Math.toRadians(180)), Math.toRadians(-90)) // push 2
+//
+////                .setTangent(Math.toRadians(90))
+//                .splineToLinearHeading(new Pose2d(43, -30, Math.toRadians(180)), Math.toRadians(90))
+//                .splineToLinearHeading(new Pose2d(56, -14, Math.toRadians(180)), Math.toRadians(0))
+//                .setTangent(Math.toRadians(-90))
+//                .splineToLinearHeading(new Pose2d(56, -54, Math.toRadians(180)), Math.toRadians(-90)) // push 3
+//
+////                .setTangent(Math.toRadians(90))
+//                .splineToLinearHeading(new Pose2d(54, -30, Math.toRadians(180)), Math.toRadians(90))
+//                .splineToLinearHeading(new Pose2d(64.5, -14, Math.toRadians(180)), Math.toRadians(0))
+//                .setTangent(Math.toRadians(-90))
+//                .splineToLinearHeading(new Pose2d(64.5, -54, Math.toRadians(180)), Math.toRadians(-90)) // push 4
+//
+////                .setTangent(Math.toRadians(90))
+//                .splineToSplineHeading(new Pose2d(52, -47, Math.toRadians(90)), Math.toRadians(180))
+//                .splineToSplineHeading(new Pose2d(46.5, -62, Math.toRadians(90)), Math.toRadians(-90)) // pick 2
+//
+//                .strafeToLinearHeading(new Vector2d(1, -28.75), Math.toRadians(90)) // place 2
+//
+//                .strafeToLinearHeading(new Vector2d(46.5, -62), Math.toRadians(90)) // pick 3
+//
+//                .strafeToLinearHeading(new Vector2d(0.5, -28.75), Math.toRadians(90)) // place 3
+//
+//                .strafeToLinearHeading(new Vector2d(46.5, -62), Math.toRadians(90)) // pick 4
+//
+//                .strafeToLinearHeading(new Vector2d(0, -28.75), Math.toRadians(90)) // place 4
+//
+//                .strafeToLinearHeading(new Vector2d(46.5, -62), Math.toRadians(90)) // pick 5
+//
+//                .strafeToLinearHeading(new Vector2d(-0.5, -28.75), Math.toRadians(90)) // place 5
+//
+//                .strafeToLinearHeading(new Vector2d(32, -60), Math.toRadians(90)) // park
+//
+//
+//                .build());
         
         RoadRunnerBotEntity specimen3Bot = new DefaultBotBuilder(meepMeep)
             .setDimensions(16, 16)
